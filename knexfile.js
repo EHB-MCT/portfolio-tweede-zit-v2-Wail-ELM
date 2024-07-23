d14d922b1c7b0eb75738021e760b1255b4df5935
Wail El Majdoubi <91526221+Wail-ELM@users.noreply.github.com>
2024-07-23 22:07:03 +0200

    feat: Database setup and brought changes knexfile.js and docker-composes

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,28 @@
 require("dotenv").config();
+
 module.exports = {
   development: {
     client: "pg",
     connection: {
+      host: '127.0.0.1', // Use IPv4 localhost address
+      port: 5432,        // Default PostgreSQL port
       database: process.env.DEV_DB_NAME,
       user: process.env.DEV_DB_USER,
       password: process.env.DEV_DB_PASSWORD,
+    },
+    migrations: {
+      directory: "./src/migrations",
+    },
+    seeds: {
+      directory: "./src/seeds",
+    },
+  },
+  production: {
+    client: "pg",
+    connection: {
+      database: process.env.PROD_DB_NAME,
+      user: process.env.PROD_DB_USER,
+      password: process.env.PROD_DB_PASSWORD,
     },
     migrations: {
       directory: "./src/migrations",
@@ -21,16 +38,6 @@ module.exports = {
       user: process.env.TEST_DB_USER,
       password: process.env.TEST_DB_PASSWORD,
     },
-    migrations: {
-      directory: "./src/migrations",
-    },
-    seeds: {
-      directory: "./src/seeds",
-    },
-  },
-  production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./src/migrations",
     },
