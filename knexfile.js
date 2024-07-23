ab3bd67ed55b5bf001a2eb060665e01a88367954
Wail El Majdoubi <91526221+Wail-ELM@users.noreply.github.com>
2024-07-23 20:07:08 +0200

    feat: modify knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,40 +1,41 @@
+require("dotenv").config();
 module.exports = {
   development: {
-    client: 'pg',
+    client: "pg",
     connection: {
-      database: 'forum',
-      user: 'user',
-      password: 'password'
+      database: process.env.DEV_DB_NAME,
+      user: process.env.DEV_DB_USER,
+      password: process.env.DEV_DB_PASSWORD,
     },
     migrations: {
-      directory: './src/migrations'
+      directory: "./src/migrations",
     },
     seeds: {
-      directory: './src/seeds'
-    }
+      directory: "./src/seeds",
+    },
   },
   test: {
-    client: 'pg',
+    client: "pg",
     connection: {
-      database: 'forum_test',
-      user: 'user',
-      password: 'password'
+      database: process.env.TEST_DB_NAME,
+      user: process.env.TEST_DB_USER,
+      password: process.env.TEST_DB_PASSWORD,
     },
     migrations: {
-      directory: './src/migrations'
+      directory: "./src/migrations",
     },
     seeds: {
-      directory: './src/seeds'
-    }
+      directory: "./src/seeds",
+    },
   },
   production: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './src/migrations'
+      directory: "./src/migrations",
     },
     seeds: {
-      directory: './src/seeds'
-    }
-  }
+      directory: "./src/seeds",
+    },
+  },
 };
